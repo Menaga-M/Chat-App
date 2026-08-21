21195c447aed10b8e56094d521cd6d76ed7f31e6
Menaga-M <menaga7221@gmail.com>
2026-08-21 18:50:21 +0530

    updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import dns from "dns";
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
+import { clerkMiddleware } from "@clerk/express";
+
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({
+    origin:FRONTEND_URL,
+    credentials:true
+}));
+app.use(clerkMiddleware())
 
 app.get("/health",(req,res) => {
     res.status(200).json({ok:true});
