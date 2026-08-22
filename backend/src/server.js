849889ebfdb85f7644a2ee2c46f73f0aa0a43c5e
Menaga-M <menaga7221@gmail.com>
2026-08-22 15:37:04 +0530

    Push

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,6 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 
-import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
