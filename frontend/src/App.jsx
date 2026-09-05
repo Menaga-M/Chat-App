32a0e7b0f8b039022262a53430f6d6d180a6f7d9
Menaga-M <menaga7221@gmail.com>
2026-09-05 15:57:39 +0530

    feat: add routing and theming support with new context providers and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import { Button } from '@heroui/react';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
+import {WallpaperProvider} from "./context/WallpaperContext";
+import {ThemeProvider} from "./context/ThemeContext";
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from "@clerk/react";
 
 function App() {
 
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if(!isLoaded){
+    return <div>Loading...</div>
+  }
   return (
-    <div>
-      <h1 className='text-4xl text-red-500 bg-blue-400'>Chat App</h1>
-      <Button>
-      My Button
-    </Button>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace/>} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace/>} />
+        </Routes>
+      </WallpaperProvider>
+      </ThemeProvider>
   )
 }
 
